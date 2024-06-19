169996fb710d1812481d2411b3c50e70edb35137
Alexander Bentkamp <bentkamp@gmail.com>
2024-06-19 20:46:10 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './style.css'
+import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div id="monaco-editor-root">
+  </div>
+`
+
+const run = async () => {
+  const wrapper = new MonacoEditorLanguageClientWrapper();
+  const userConfig : UserConfig = {
+    wrapperConfig: {
+      editorAppConfig: {
+        $type: 'extended',
+        codeResources: {
+          main: {
+            text: 'print("Hello, World!")',
+            uri: '/workspace/hello.py'
+          }
+        }
+      }
+    }
+  };
+
+  const htmlElement = document.getElementById('monaco-editor-root');
+  await wrapper.initAndStart(userConfig, htmlElement);
+}
+
+run()
