d1800615f92492f768dde4af356f6175dec4a157
Alexander Bentkamp <bentkamp@gmail.com>
2024-06-19 22:17:26 +0200

    reinit demo

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,40 @@
 import './style.css'
 import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div id="monaco-editor-root">
-  </div>
-`
+const extensionFilesOrContents = new Map<string, string | URL>();
+extensionFilesOrContents.set('/test.json', "Test");
+
+const userConfig : UserConfig = {
+  wrapperConfig: {
+    editorAppConfig: {
+      $type: 'extended',
+      codeResources: {
+        main: {
+          text: 'print("Hello, World!")',
+          uri: '/workspace/hello.py'
+        }
+      },
+      extensions: [{
+        config: {
+            name: 'myextension',
+            publisher: 'me'
+        },
+        filesOrContents: extensionFilesOrContents
+      }]
+    }
+  }
+};
 
 const run = async () => {
-  const wrapper = new MonacoEditorLanguageClientWrapper();
-  const userConfig : UserConfig = {
-    wrapperConfig: {
-      editorAppConfig: {
-        $type: 'extended',
-        codeResources: {
-          main: {
-            text: 'print("Hello, World!")',
-            uri: '/workspace/hello.py'
-          }
-        }
-      }
-    }
-  };
-
   const htmlElement = document.getElementById('monaco-editor-root');
+
+  const wrapper = new MonacoEditorLanguageClientWrapper();
   await wrapper.initAndStart(userConfig, htmlElement);
+  await wrapper.dispose();
+
+  const wrapper2 = new MonacoEditorLanguageClientWrapper();
+  await wrapper2.initAndStart(userConfig, htmlElement);
+  await wrapper2.dispose();
 }
 
 run()
